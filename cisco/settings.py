d7f3dd572758d2eec154326bb96d036643752ffc
Alberto Labrada <albertolabrada99@gmail.com>
2020-05-07 10:22:45 -0600

    django + mongo connection
    Signed-off-by: Alberto Labrada <albertolabrada99@gmail.com>

--- cisco/settings.py
+++ cisco/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'cisco.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': 'ciscoticket',
+        'HOST': 'localhost',
+        'PORT': 27017
     }
 }
 
