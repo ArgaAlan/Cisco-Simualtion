87aea5ec886dcbddfd427dd39412188a0bbc7ec0
DanielLepeVega <danylepevega@gmail.com>
2020-05-07 13:47:58 -0500

    Dashboard little changes
    Signed-off-by: DanielLepeVega <danylepevega@gmail.com>

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -143,7 +143,7 @@ export default function Dashboard() {
             <MenuIcon />
           </IconButton>
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            Dashboard
+            The Northerners Dashboard
           </Typography>
           <IconButton color="inherit">
             <Badge badgeContent={4} color="secondary">
