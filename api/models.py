97eaa15ee928e5086ed0971c7f31c0c54fa6ab9c
Alan Gonzalez <argaalan@hotmail.com>
2020-05-11 12:09:41 -0600

    API updated for new fields

--- api/models.py
+++ api/models.py
@@ -3,31 +3,32 @@ from django.db import models
 
 class Ticket(models.Model):
     numberID = models.IntegerField(primary_key=True)
+    impactedUser = models.CharField(
+        max_length=200, default='', blank=True, null=True)
     subclass = models.CharField(
         max_length=200, default='', blank=True, null=True)
     category = models.CharField(
         max_length=200, default='', blank=True, null=True)
-    lifecycle = models.CharField(
+    state = models.CharField(
         max_length=200, default='', blank=True, null=True)
-    description = models.CharField(
+    summary = models.CharField(
         max_length=200, default='', blank=True, null=True)
-    incorpDate = models.DateTimeField(
+    causingCI = models.CharField(
+        max_length=200, default='', blank=True, null=True)
+    assignedTo = models.CharField(
+        max_length=200, default='', blank=True, null=True)
+    openDate = models.DateTimeField(
         default='2020-01-01 00:00', blank=True, null=True)
-    releaseDate = models.DateTimeField(
+    assignedDate = models.DateTimeField(
         default='2020-01-01 00:00', blank=True, null=True)
-    effectivityDate = models.DateTimeField(
+    resolutionDate = models.DateTimeField(
         default='2020-01-01 00:00', blank=True, null=True)
-    complianceCalculatedDate = models.DateTimeField(
+    closedDate = models.DateTimeField(
         default='2020-01-01 00:00', blank=True, null=True)
-    overallCompliance = models.CharField(
+    escalationDate = models.DateTimeField(
+        default='2020-01-01 00:00', blank=True, null=True)
+    notes = models.CharField(
         max_length=200, default='', blank=True, null=True)
-    levelComplianceIndicator = models.CharField(
-        max_length=200, default='', blank=True, null=True)
-    complianceRollUp = models.CharField(
-        max_length=200, default='', blank=True, null=True)
-    productHierarchy = models.CharField(
-        max_length=200, default='', blank=True, null=True)
-    user = models.CharField(max_length=200, default='', blank=True, null=True)
 
     def __str__(self):
         return self.numberID
