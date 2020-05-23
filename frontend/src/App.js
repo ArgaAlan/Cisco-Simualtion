740f54031d0cff242c9b0344fe422d6548e3d654
DanielLepeVega <danylepevega@gmail.com>
2020-05-23 15:06:08 -0500

    Sign In
    Signed-off-by: DanielLepeVega <danylepevega@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,18 @@ import Dashboard from './components/Dashboard';
 import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './theme';
 import './App.css';
+import SignIn from "./components/SignIn"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/signin/" component={SignIn}></Route>
+          <Route path="/" component={Dashboard}></Route>
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
