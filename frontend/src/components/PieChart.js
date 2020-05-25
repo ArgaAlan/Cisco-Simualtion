cdf7d64a6440eca2987542d9ce29f156c054530e
Alberto Labrada <albertolabrada99@gmail.com>
2020-05-25 13:37:08 -0600

    state ticket
    Signed-off-by: Alberto Labrada <albertolabrada99@gmail.com>

--- frontend/src/components/PieChart.js
+++ frontend/src/components/PieChart.js
@@ -7,38 +7,79 @@ import {
 } from "@devexpress/dx-react-chart-material-ui";
 
 import { Animation } from "@devexpress/dx-react-chart";
-
-const data = [
-  { country: "Russia", area: 12 },
-  { country: "Canada", area: 7 },
-  { country: "USA", area: 7 },
-  { country: "China", area: 7 },
-  { country: "Brazil", area: 6 },
-  { country: "Australia", area: 5 },
-  { country: "India", area: 2 },
-  { country: "Others", area: 55 },
-];
-
-const title = "Hola";
+import axios from 'axios';
 
 export default class Demo extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      data,
-      title,
+      data: {
+        regression: {
+          materialLeadTime: {
+            transportIssue: [],
+            demandSurge: [],
+            productDesignChange: []
+          },
+          materialQuantity: {
+            yieldIssue: [],
+            scrapDueToEco: [],
+          },
+          materialQuality: {
+            manufacturingIssue: [],
+            designIssue: [],
+            processIssue: [],
+            trainingIssue: []
+          }
+        }
+      },
+      title: "Paises",
+      data2: [
+        { country: "Russia", area: 12 },
+        { country: "Canada", area: 7 },
+        { country: "USA", area: 7 },
+        { country: "China", area: 7 },
+        { country: "Brazil", area: 6 },
+        { country: "Australia", area: 5 },
+        { country: "India", area: 2 },
+        { country: "Others", area: 55 },
+      ],
+      loading: false,
     };
   }
 
-  render() {
-    const { data: chartData } = this.state;
+  componentDidMount() {
+    this.getData();
+  }
 
+  getData = async () => {
+    this.setState({ loading: true });
+    const res = await axios.get('http://localhost:8000/simulation');
+    this.setState({ data: res.data, loading: false });
+    console.log(res.data);
+  };
+
+  render() {
+    if (this.state.loading == true) {
+      return <h1>Loading</h1>;
+    }
+    console.log(this.state.data);
+    console.log(this.state.data.regression.materialLeadTime);
     return (
       <Paper>
-        <Chart data={chartData}>
+        <Chart data={this.state.data2}>
           <PieSeries valueField="area" argumentField="country" />
-          <Title text={title} />
+          <Title text={this.state.title} />
+          <Animation />
+        </Chart>
+        <Chart data={this.state.data2}>
+          <PieSeries valueField="area" argumentField="country" />
+          <Title text={this.state.title} />
+          <Animation />
+        </Chart>
+        <Chart data={this.state.data2}>
+          <PieSeries valueField="area" argumentField="country" />
+          <Title text={this.state.title} />
           <Animation />
         </Chart>
       </Paper>
