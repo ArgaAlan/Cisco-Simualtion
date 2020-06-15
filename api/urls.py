16416394a9430609edd9935180bf3c8630c65099
Alan Gonzalez <argaalan@hotmail.com>
2020-06-15 11:58:04 -0600

    Django deleted

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.apiOverview, name="api-overview"),
-    path('tickets/', views.ticketList, name="ticket-list"),
-    path('tickets/<str:pk>', views.ticketDetail, name="ticket-detail"),
-]
