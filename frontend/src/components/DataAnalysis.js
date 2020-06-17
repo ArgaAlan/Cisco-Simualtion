7492557938ec82b67aaaaa6ee9907bbfeb236d8e
Christopher Ortega <A01634500@itesm.mx>
2020-06-17 15:09:57 -0600

    first workininig chart

--- frontend/src/components/DataAnalysis.js
+++ frontend/src/components/DataAnalysis.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+/*import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
 import {
   Chart,
@@ -14,22 +14,22 @@ import {
 } from '@devexpress/dx-react-chart-material-ui';
 
 import {
+  ValueScale,
   Stack,
   Animation,
   EventTracker,
-  ArgumentScale,
   HoverState,
 } from '@devexpress/dx-react-chart';
 import { HOVERED } from '@devexpress/dx-chart-core';
 import { connectProps } from '@devexpress/dx-react-core';
-import { scaleBand } from '@devexpress/dx-chart-core';
 import { format } from 'd3-format';
 
 import { ChartInfo } from './tmpDummyData/data';
 
-const totalReports = 'Total reports';
-const reportColor = '#41c0f0';
+const consumptionSeriesName = 'Monthly report';
+const consumptionColor = '#41c0f0';
 const priceColor = '#fcd45a';
+
 
 const makeLabel = (symbol, color) => ({ text, style, ...restProps }) => (
   <ValueAxis.Label
@@ -41,8 +41,7 @@ const makeLabel = (symbol, color) => ({ text, style, ...restProps }) => (
     {...restProps}
   />
 );
-const PriceLabel = makeLabel('$', priceColor);
-const LabelWithThousand = makeLabel('k', reportColor);
+const PriceLabel = makeLabel('', priceColor);
 
 const patchProps = ({ hoverIndex, ...props }) => ({
   state: props.index === hoverIndex ? HOVERED : null,
@@ -80,12 +79,12 @@ const SplineWithPoints = props => (
 );
 
 const series = [
-  { name: 'Due date', key: 'due', color: '#08abbd' },
-  { name: 'Quantity', key: 'quan', color: '#78bc97' },
-  { name: 'Quality', key: 'qual', color: '#d4d67e' },
+  { name: 'Late arrival', key: 'dueDate', color:'#08abbd'},
+  { name: 'Low quality', key: 'quality', color:'#78bc97'},
+  { name: 'Less quantity', key: 'quantity', color:'#d4d67e'},
   {
-    name: totalReports, key: 'total', color: reportColor, type: AreaSeries,
-  },
+    name: 'Total', key: 'total', color: priceColor, scale: 'price', type: SplineSeries,
+  }
 ];
 
 const legendRootStyle = {
@@ -150,8 +149,8 @@ const stacks = [
   { series: series.filter(obj => !obj.type).map(obj => obj.name) },
 ];
 
-/*const modifyOilDomain = domain => [domain[0], 2200];
-const modifyPriceDomain = domain => [domain[0], 110];*/
+const modifyOilDomain = domain => [domain[0], 2200];
+const modifyPriceDomain = () => [0, 20];
 
 const getHoverIndex = ({ target }) => (target ? target.point : -1);
 
@@ -204,9 +203,9 @@ function parseToJSON(monthlyTkt){
 
         data[i].append({
             'month': i,
-            'due': due,
-            'qual': qual,
-            'quan': quan,
+            'dueDate': due,
+            'quality': qual,
+            'quantity': quan,
             'total': total
         })
     }
@@ -214,122 +213,371 @@ function parseToJSON(monthlyTkt){
 }
 
 export default class DataAnalysis extends React.PureComponent {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          data: ChartInfo,
-          target: null,
-        };
-    
-        this.changeHover = target => this.setState({
-          target: target ? { series: totalReports, point: target.point } : null,
-        });
-    
-        this.createComponents();
-        this.createSeries();
-      }
-    
-      componentDidUpdate(prevProps, prevState) {
-        if (getHoverIndex(prevState) !== getHoverIndex(this.state)) {
-          this.BarPoint.update();
-          this.SplinePoint.update();
-          this.AreaPoint.update();
-          this.TooltipContent.update();
-        }
-      }
-    
-      createComponents() {
-        const getHoverProps = () => ({
-          hoverIndex: getHoverIndex(this.state),
-        });
-        this.BarPoint = connectProps(BarPoint, getHoverProps);
-        this.SplinePoint = connectProps(SplinePoint, getHoverProps);
-        this.AreaPoint = connectProps(AreaPoint, getHoverProps);
-    
-        this.TooltipContent = connectProps(TooltipContent, () => {
-          const { data, target } = this.state;
-          return { data: target ? data[target.point] : null };
-        });
-      }
-    
-      createSeries() {
-        this.series = series.map(({
-          name, key, color, type, scale,
-        }) => {
-          const props = {
-            key: name,
-            name,
-            valueField: key,
-            argumentField: 'year',
-            color,
-            scaleName: scale || 'reports',
-            pointComponent: this.BarPoint,
-          };
-          if (type === AreaSeries) {
-            props.seriesComponent = AreaWithPoints;
-            props.pointComponent = this.AreaPoint;
-          } else if (type) {
-            props.seriesComponent = SplineWithPoints;
-            props.pointComponent = this.SplinePoint;
-          }
-          return React.createElement(type || BarSeries, props);
-        });
-      }
-    
-/*<ValueScale
-                name="oil"
-                modifyDomain={modifyOilDomain}
-              />
-              <ValueScale
-                name="price"
-                modifyDomain={modifyPriceDomain}
-              />*/
+  constructor(props) {
+    super(props);
 
-      render() {
-        const { data, target } = this.state;
-    
-        return (
-          <Paper>
-            <Chart
-              data={data}
-            >
-            <ArgumentScale factory={scaleBand} />
-              <ValueAxis
-                scaleName="oil"
-                labelComponent={LabelWithThousand}
-              />
-              <ValueAxis
-                scaleName="price"
-                position="right"
-                labelComponent={PriceLabel}
-              />
-    
-              <Title
-                text="Anual reports"
-              />
-    
-              {this.series}
-    
-              <Animation />
-              <Legend
-                position="bottom"
-                rootComponent={LegendRoot}
-                itemComponent={LegendItem}
-                labelComponent={LegendLabel}
-              />
-              <Stack stacks={stacks} />
-              <EventTracker />
-              <HoverState
-                hover={target}
-                onHoverChange={this.changeHover}
-              />
-              <Tooltip
-                targetItem={target}
-                contentComponent={this.TooltipContent}
-              />
-            </Chart>
-          </Paper>
-        );
+    this.state = {
+      data: ChartInfo,
+      target: null,
+    };
+
+    this.changeHover = target => this.setState({
+      target: target ? { series: consumptionSeriesName, point: target.point } : null,
+    });
+
+    this.createComponents();
+    this.createSeries();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (getHoverIndex(prevState) !== getHoverIndex(this.state)) {
+      this.BarPoint.update();
+      this.SplinePoint.update();
+      this.AreaPoint.update();
+      this.TooltipContent.update();
+    }
+  }
+
+  createComponents() {
+    const getHoverProps = () => ({
+      hoverIndex: getHoverIndex(this.state),
+    });
+    this.BarPoint = connectProps(BarPoint, getHoverProps);
+    this.SplinePoint = connectProps(SplinePoint, getHoverProps);
+    this.AreaPoint = connectProps(AreaPoint, getHoverProps);
+
+    this.TooltipContent = connectProps(TooltipContent, () => {
+      const { data, target } = this.state;
+      return { data: target ? data[target.point] : null };
+    });
+  }
+
+  createSeries() {
+    this.series = series.map(({
+      name, key, color, type, scale,
+    }) => {
+      const props = {
+        key: name,
+        name,
+        valueField: key,
+        argumentField: 'month',
+        color,
+        scaleName: scale || 'oil',
+        pointComponent: this.BarPoint,
+      };
+      if (type === AreaSeries) {
+        props.seriesComponent = AreaWithPoints;
+        props.pointComponent = this.AreaPoint;
+      } else if (type) {
+        props.seriesComponent = SplineWithPoints;
+        props.pointComponent = this.SplinePoint;
       }
+      return React.createElement(type || BarSeries, props);
+    });
+  }
+
+  render() {
+    const { data, target } = this.state;
+
+    return (
+      <Paper>
+        <Chart
+          data={data}
+        >
+          <ValueScale
+            name="oil"
+            modifyDomain={modifyOilDomain}
+          />
+          <ValueScale
+            name="price"
+            modifyDomain={modifyPriceDomain}
+          />
+
+          <ArgumentAxis />
+          <ValueAxis
+            scaleName="price"
+            position="left"
+            labelComponent={PriceLabel}
+          />
+
+          <Title
+            text="Monthly reports"
+          />
+
+          {this.series}
+
+          <Animation />
+          <Legend
+            position="bottom"
+            rootComponent={LegendRoot}
+            itemComponent={LegendItem}
+            labelComponent={LegendLabel}
+          />
+          <Stack stacks={stacks} />
+          <EventTracker />
+          <HoverState
+            hover={target}
+            onHoverChange={this.changeHover}
+          />
+          <Tooltip
+            targetItem={target}
+            contentComponent={this.TooltipContent}
+          />
+        </Chart>
+      </Paper>
+    );
+  }
+}*/
+import * as React from "react";
+import Paper from "@material-ui/core/Paper";
+import {
+  Chart,
+  AreaSeries,
+  BarSeries,
+  SplineSeries,
+  ScatterSeries,
+  ArgumentAxis,
+  ValueAxis,
+  Title,
+  Legend,
+  Tooltip
+} from "@devexpress/dx-react-chart-material-ui";
+
+import {
+  ValueScale,
+  Stack,
+  Animation,
+  EventTracker,
+  HoverState
+} from "@devexpress/dx-react-chart";
+import { HOVERED } from "@devexpress/dx-chart-core";
+import { connectProps } from "@devexpress/dx-react-core";
+import { format } from "d3-format";
+
+import { ChartInfo } from "./tmpDummyData/data";
+
+const consumptionSeriesName = "Consumption";
+const consumptionColor = "#41c0f0";
+const priceColor = "#fcd45a";
+
+const makeLabel = (symbol, color) => ({ text, style, ...restProps }) => (
+  <ValueAxis.Label
+    text={`${text} ${symbol}`}
+    style={{
+      fill: color,
+      ...style
+    }}
+    {...restProps}
+  />
+);
+const PriceLabel = makeLabel("$", priceColor);
+const LabelWithThousand = makeLabel("k", consumptionColor);
+
+const patchProps = ({ hoverIndex, ...props }) => ({
+  state: props.index === hoverIndex ? HOVERED : null,
+  ...props
+});
+
+const BarPoint = props => <BarSeries.Point {...patchProps(props)} />;
+
+const pointOptions = { size: 7 };
+const getPointOptions = state => (state ? { size: 7 } : { size: 0 });
+
+const AreaPoint = props => {
+  const patched = patchProps(props);
+  return (
+    <ScatterSeries.Point point={getPointOptions(patched.state)} {...patched} />
+  );
+};
+
+const AreaWithPoints = ({ state, ...props }) => (
+  <React.Fragment>
+    <AreaSeries.Path {...props} />
+    <ScatterSeries.Path {...props} />
+  </React.Fragment>
+);
+
+const SplinePoint = props => (
+  <ScatterSeries.Point point={pointOptions} {...patchProps(props)} />
+);
+
+const SplineWithPoints = props => (
+  <React.Fragment>
+    <SplineSeries.Path {...props} />
+    <ScatterSeries.Path {...props} />
+  </React.Fragment>
+);
+
+const series = [
+  { name: "Late arrival", key: "dueDate", color: "#08abbd" },
+  { name: "Low quality", key: "quality", color: "#78bc97" },
+  { name: "Less quantity", key: "quantity", color: "#d4d67e" },
+  {
+    name: "Total",
+    key: "total",
+    color: priceColor,
+    type: SplineSeries
+  }
+];
+
+const legendRootStyle = {
+  display: "flex",
+  margin: "auto",
+  flexDirection: "row"
+};
+const LegendRoot = props => <Legend.Root {...props} style={legendRootStyle} />;
+
+const legendItemStyle = {
+  flexDirection: "column",
+  marginLeft: "-2px",
+  marginRight: "-2px"
+};
+const LegendItem = props => <Legend.Item {...props} style={legendItemStyle} />;
+
+const legendLabelStyle = {
+  whiteSpace: "nowrap"
+};
+const LegendLabel = props => (
+  <Legend.Label {...props} style={legendLabelStyle} />
+);
+
+const formatTooltip = format(".1f");
+const TooltipContent = ({ data, text, style, ...props }) => {
+  const alignStyle = {
+    ...style,
+    paddingLeft: "10px"
+  };
+  const items = series.map(({ name, key, color }) => {
+    const val = data[key];
+    return (
+      <tr key={key}>
+        <td>
+          <svg width="10" height="10">
+            <circle cx="5" cy="5" r="5" fill={color} />
+          </svg>
+        </td>
+        <td>
+          <Tooltip.Content style={alignStyle} text={name} {...props} />
+        </td>
+        <td align="right">
+          <Tooltip.Content
+            style={alignStyle}
+            text={val ? formatTooltip(val) : "N/A"}
+            {...props}
+          />
+        </td>
+      </tr>
+    );
+  });
+  return <table>{items}</table>;
+};
+
+const stacks = [
+  { series: series.filter(obj => !obj.type).map(obj => obj.name) }
+];
+
+const modifyOilDomain = domain => [domain[0], 20];
+const modifyPriceDomain = () => [0, 20];
+
+const getHoverIndex = ({ target }) => (target ? target.point : -1);
+
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: ChartInfo,
+      target: null
+    };
+
+    this.changeHover = target =>
+      this.setState({
+        target: target
+          ? { series: consumptionSeriesName, point: target.point }
+          : null
+      });
+
+    this.createComponents();
+    this.createSeries();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (getHoverIndex(prevState) !== getHoverIndex(this.state)) {
+      this.BarPoint.update();
+      this.SplinePoint.update();
+      this.AreaPoint.update();
+      this.TooltipContent.update();
+    }
+  }
+
+  createComponents() {
+    const getHoverProps = () => ({
+      hoverIndex: getHoverIndex(this.state)
+    });
+    this.BarPoint = connectProps(BarPoint, getHoverProps);
+    this.SplinePoint = connectProps(SplinePoint, getHoverProps);
+    this.AreaPoint = connectProps(AreaPoint, getHoverProps);
+
+    this.TooltipContent = connectProps(TooltipContent, () => {
+      const { data, target } = this.state;
+      return { data: target ? data[target.point] : null };
+    });
+  }
+
+  createSeries() {
+    this.series = series.map(({ name, key, color, type, scale }) => {
+      const props = {
+        key: name,
+        name,
+        valueField: key,
+        argumentField: "month",
+        color,
+        pointComponent: this.BarPoint
+      };
+      if (type === AreaSeries) {
+        props.seriesComponent = AreaWithPoints;
+        props.pointComponent = this.AreaPoint;
+      } else if (type) {
+        props.seriesComponent = SplineWithPoints;
+        props.pointComponent = this.SplinePoint;
+      }
+      return React.createElement(type || BarSeries, props);
+    });
+  }
+
+  render() {
+
+    return (
+      <Paper>
+        <Chart data={this.state.data}>
+          <ValueScale name="oil" modifyDomain={modifyOilDomain} />
+          <ValueScale name="price" modifyDomain={modifyPriceDomain} />
+
+          <ArgumentAxis />
+          <ValueAxis scaleName="oil" labelComponent={LabelWithThousand} />
+          <ValueAxis
+            scaleName="price"
+            position="right"
+            labelComponent={PriceLabel}
+          />
+
+          <Title text="Monthly reports" />
+
+          {this.series}
+
+          <Animation />
+          <Legend
+            position="bottom"
+            rootComponent={LegendRoot}
+            itemComponent={LegendItem}
+            labelComponent={LegendLabel}
+          />
+          <Stack stacks={stacks} />
+          <EventTracker />
+          <HoverState hover={this.state.target} onHoverChange={this.changeHover} />
+          <Tooltip targetItem={this.state.target} contentComponent={this.TooltipContent} />
+        </Chart>
+      </Paper>
+    );
+  }
 }
--- frontend/src/components/tmpDummyData/data.js
+++ frontend/src/components/tmpDummyData/data.js
@@ -1,15 +1,41 @@
 export const ChartInfo = [
     {
-      date: '02-08-2019', causingCI: 'DueDate'
+      month: 0, dueDate: 5, quality: 3, quantity: 9, total: 17
     }, {
-      year: '1980', saudiArabia: 511.334, usa: 437.343, iran: 75.097, mexico: 108.249, price: 104, consumption: 630,
+      month: 1, dueDate: 1, quality: 1, quantity: 2, total: 4
     }, {
-      year: '1990', saudiArabia: 324.359, usa: 374.867, iran: 165.284, mexico: 141.060, russia: 516.040, price: 23.7, consumption: 590,
+      month: 2, dueDate: 2, quality: 1, quantity: 3, total: 6
     }, {
-      year: '2000', saudiArabia: 410.060, usa: 297.513, iran: 196.877, mexico: 159.630, russia: 312.821, price: 28.3, consumption: 680,
+      month: 3, dueDate: 4, quality: 0, quantity: 5, total: 9
     }, {
-      year: '2010', saudiArabia: 413.505, usa: 279.225, iran: 200.318, mexico: 144.975, russia: 487.106, price: 79.6, consumption: 640,
+      month: 4, dueDate: 0, quality: 0, quantity: 1, total: 1
     }, {
-      year: '2015', saudiArabia: 516.157, usa: 437.966, iran: 142.087, mexico: 121.587, russia: 512.777, price: 52.4, consumption: 790,
-    },
+      month: 5, dueDate: 1, quality: 2, quantity: 0, total: 3
+    }, {
+      month: 6, dueDate: 2, quality: 0, quantity: 4, total: 6
+    }, {
+      month: 7, dueDate: 4, quality: 0, quantity: 2, total: 6
+    }, {
+      month: 8, dueDate: 5, quality: 1, quantity: 1, total: 7
+    }, {
+      month: 9, dueDate: 3, quality: 1, quantity: 0, total: 4
+    }, {
+      month: 10, dueDate: 8, quality: 2, quantity: 1, total: 11
+    }, {
+      month: 11, dueDate: 10, quality: 0, quantity: 3, total: 13
+    }
   ];
+
+  /*
+  , {
+    year: '1980', saudiArabia: 511.334, usa: 437.343, iran: 75.097, mexico: 108.249, price: 104, consumption: 630,
+  }, {
+    year: '1990', saudiArabia: 324.359, usa: 374.867, iran: 165.284, mexico: 141.060, russia: 516.040, price: 23.7, consumption: 590,
+  }, {
+    year: '2000', saudiArabia: 410.060, usa: 297.513, iran: 196.877, mexico: 159.630, russia: 312.821, price: 28.3, consumption: 680,
+  }, {
+    year: '2010', saudiArabia: 413.505, usa: 279.225, iran: 200.318, mexico: 144.975, russia: 487.106, price: 79.6, consumption: 640,
+  }, {
+    year: '2015', saudiArabia: 516.157, usa: 437.966, iran: 142.087, mexico: 121.587, russia: 512.777, price: 52.4, consumption: 790,
+  }, 
+  */
