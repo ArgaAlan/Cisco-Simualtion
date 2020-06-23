b126bc0d8e1702540113111dc97846b69dd635c4
Alan Gonzalez <argaalan@hotmail.com>
2020-06-22 18:58:29 -0600

    Bayesnet simulation working

--- src/components/Simulation.js
+++ src/components/Simulation.js
@@ -1,9 +1,9 @@
-import React, { useContext } from "react";
-import Title from "./Title";
+import React, { useContext, useState } from "react";
 import { Context } from "../context/user/userContext";
-import { simulationContext } from "../context/simulation/simulationContext";
 import { Redirect } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Title from "./Title";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -11,18 +11,75 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import clsx from "clsx";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Grid from "@material-ui/core/Grid";
+import SaveIcon from "@material-ui/icons/Save";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import TextField from "@material-ui/core/TextField";
+import { useAuth0 } from "../react-auth0-spa";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  withoutLabel: {
+    marginTop: theme.spacing(3),
+  },
+  textField: {
+    width: "25ch",
+  },
   table: {
     minWidth: 650,
   },
-});
+}));
 
 export default function Simulation() {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
 
   const [privilege, setPrivilege] = useContext(Context);
-  const [simulation, setSimulation] = useContext(simulationContext);
+  const [state, setState] = useState({
+    mlt_risk_0: 0.2,
+    mlt_risk_1: 0.2,
+    mlt_risk_2: 0.1,
+    mlt_risk_3: 0.5,
+    mqnt_risk_0: 0.3,
+    mqnt_risk_1: 0.3,
+    mqnt_risk_2: 0.4,
+    mqlt_risk_0: 0.1,
+    mqlt_risk_1: 0.05,
+    mqlt_risk_2: 0.25,
+    mqlt_risk_3: 0.4,
+    mqlt_risk_4: 0.2,
+  });
+
+  var mlt_risk = [
+    [state.mlt_risk_0, state.mlt_risk_1, state.mlt_risk_2, state.mlt_risk_3],
+    [],
+  ];
+  var mqnt_risk = [
+    [state.mqnt_risk_0, state.mqnt_risk_1, state.mqnt_risk_2],
+    [0, 0, 1],
+  ];
+  var mqlt_risk = [
+    [
+      state.mqlt_risk_0,
+      state.mqlt_risk_1,
+      state.mqlt_risk_2,
+      state.mqlt_risk_3,
+      state.mqlt_risk_4,
+    ],
+    [0, 0, 0, 0, 1],
+  ];
 
   var months = 12;
 
@@ -65,11 +122,7 @@ export default function Simulation() {
 
     for (var i = 0; i < 420; i++) {
       var values = [];
-      values = bayesian(
-        simulation.bayesian.mlt_risk,
-        simulation.bayesian.mqnt_risk,
-        simulation.bayesian.mqlt_risk
-      );
+      values = bayesian(mlt_risk, mqnt_risk, mqlt_risk);
 
       switch (values[0]) {
         case "Fail due to Material Lead Time Risk: Transport Issue":
@@ -119,11 +172,11 @@ export default function Simulation() {
     ];
   };
 
-  const bayesian = (mlt_risk_context, mqnt_risk_context, mqlt_risk_context) => {
+  const bayesian = (mlt_risk_input, mqnt_risk_input, mqlt_risk_input) => {
     // Main simulation
-    const mlt_risk = mlt_risk_context;
-    const mqnt_risk = mqnt_risk_context;
-    const mqlt_risk = mqlt_risk_context;
+    const mlt_risk = mlt_risk_input;
+    const mqnt_risk = mqnt_risk_input;
+    const mqlt_risk = mqlt_risk_input;
 
     var output, rand, failure, l, u, i, probs;
 
@@ -266,43 +319,9 @@ export default function Simulation() {
       sumMaterialQualityTraining += values[8];
       sumMaterialQuality += values[8];
     }
-
-    console.log("CREATING DATA");
-    console.log(mlt_count_ti_periods);
-    console.log(mlt_count_ds_periods);
-    console.log(mlt_count_pdc_periods);
-    console.log(mqnt_count_yi_periods);
-    console.log(mqnt_count_se_periods);
-    console.log(mqlt_count_mi_periods);
-    console.log(mqlt_count_di_periods);
-    console.log(mqlt_count_pi_periods);
-    console.log(mqlt_count_ti_periods);
-    console.log("Lead Time");
-    console.log(sumMaterialLeadTimeDemand);
-    console.log(sumMaterialLeadTimeDesign);
-    console.log(sumMaterialLeadTimeTransport);
-    console.log("Quality");
-    console.log(sumMaterialQualityManufaturing);
-    console.log(sumMaterialQualityDesign);
-    console.log(sumMaterialQualityProcess);
-    console.log(sumMaterialQualityTraining);
-    console.log("Quantity");
-    console.log(sumMaterialQuantityYield);
-    console.log(sumMaterialQuantityScrap);
-    console.log("CREATING DATA");
-    console.log("Lead Time");
-    console.log(sumMaterialLeadTime);
-    console.log("Quality");
-    console.log(sumMaterialQuality);
-    console.log("Quantity");
-    console.log(sumMaterialQuantity);
   };
 
   createData();
-
-  if (privilege != "Analyst" && privilege != "Admin") {
-    return <Redirect to="" />;
-  }
 
   function createDataRows(name, num) {
     return { name, num };
@@ -329,78 +348,294 @@ export default function Simulation() {
     createDataRows("Quantity Issue", sumMaterialQuantity),
   ];
 
-  return (
-    <React.Fragment>
-      <Title>Data created: Lead Time Issues</Title>
-      <br />
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Type of Issue</TableCell>
-              <TableCell align="right">Number of Issues</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rowsLeadTime.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.num}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <br />
-      <br />
-      <Title>Data created: Quality Issues</Title>
-      <br />
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Type of Issue</TableCell>
-              <TableCell align="right">Number of Issues</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rowsQuality.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.num}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <br />
-      <br />
-      <Title>Data created: Quantity Issues</Title>
-      <br />
-      <TableContainer component={Paper}>
-        <Table className={classes.table} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Type of Issue</TableCell>
-              <TableCell align="right">Number of Issues</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rowsQuantity.map((row) => (
-              <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
-                  {row.name}
-                </TableCell>
-                <TableCell align="right">{row.num}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </React.Fragment>
-  );
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (prop) => (event) => {
+    setState({ ...state, [prop]: parseFloat(event.target.value) });
+  };
+
+  const handleSubmit = () => {
+    setOpen(false);
+  };
+
+  const [data, setData] = useState("Bayesnet");
+
+  if (privilege != "Analyst" && privilege != "Admin") {
+    return <Redirect to="" />;
+  }
+
+  const handleClick = () => {
+    if (data === "Bayesnet") {
+      setData("Database");
+    } else if (data === "Database") {
+      setData("Bayesnet");
+    }
+  };
+
+  if (data === "Bayesnet") {
+    return (
+      <div>
+        <React.Fragment>
+          <div>
+            <Grid container justify="space-between">
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  onClick={handleClick}
+                  align="left"
+                >
+                  Change data
+                </Button>
+              </Grid>
+              <Grid item>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  onClick={handleClickOpen}
+                  align="right"
+                >
+                  Change Probabilities
+                </Button>
+              </Grid>
+            </Grid>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="form-dialog-title"
+              fullScreen
+            >
+              <DialogTitle id="form-dialog-title">Probabilities</DialogTitle>
+              <DialogContent>
+                <div className={classes.root}>
+                  <div>
+                    {/* TRANSPORT */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Transport Issue"
+                      type="number"
+                      value={state.mlt_risk_0}
+                      onChange={handleChange("mlt_risk_0")}
+                    />
+                    {/* DEMAND */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Demand Surge"
+                      type="number"
+                      value={state.mlt_risk_1}
+                      onChange={handleChange("mlt_risk_1")}
+                    />
+                    {/* DESIGN */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Product Design Change"
+                      type="number"
+                      value={state.mlt_risk_2}
+                      onChange={handleChange("mlt_risk_2")}
+                    />
+                    {/* NO */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability No Lead Time Issues"
+                      type="number"
+                      value={state.mlt_risk_3}
+                      onChange={handleChange("mlt_risk_3")}
+                    />
+                    <br />
+                    {/* YIELD */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Yield Issue"
+                      type="number"
+                      value={state.mqnt_risk_0}
+                      onChange={handleChange("mqnt_risk_0")}
+                    />
+                    {/* SCRAP */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Scrap due to ECO"
+                      type="number"
+                      value={state.mqnt_risk_1}
+                      onChange={handleChange("mqnt_risk_1")}
+                    />
+                    {/* NO */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability No Quantity Issues"
+                      type="number"
+                      value={state.mqnt_risk_2}
+                      onChange={handleChange("mqnt_risk_2")}
+                    />
+                    <br />
+                    {/* MANUFACTURING */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Manufacturing Issue"
+                      type="number"
+                      value={state.mqlt_risk_0}
+                      onChange={handleChange("mqlt_risk_0")}
+                    />
+                    {/* DESIGN */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Design Issue"
+                      type="number"
+                      value={state.mqlt_risk_1}
+                      onChange={handleChange("mqlt_risk_1")}
+                    />
+                    {/* PROCESS */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Process Issue"
+                      type="number"
+                      value={state.mqlt_risk_2}
+                      onChange={handleChange("mqlt_risk_2")}
+                    />
+                    {/* TRAINING */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability Training Issue"
+                      type="number"
+                      value={state.mqlt_risk_3}
+                      onChange={handleChange("mqlt_risk_3")}
+                    />
+                    {/* NO */}
+                    <TextField
+                      className={clsx(classes.margin, classes.textField)}
+                      autoFocus
+                      margin="normal"
+                      label="Probability No Lead Time Issues"
+                      type="number"
+                      value={state.mqlt_risk_4}
+                      onChange={handleChange("mqlt_risk_4")}
+                    />
+                    <br />
+                  </div>
+                </div>
+              </DialogContent>
+              <DialogActions>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  className={classes.button}
+                  startIcon={<SaveIcon />}
+                  onClick={handleSubmit}
+                >
+                  Save
+                </Button>
+              </DialogActions>
+            </Dialog>
+          </div>
+          <br />
+          <Title>Data created: Lead Time Issues</Title>
+          <br />
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Type of Issue</TableCell>
+                  <TableCell align="right">Number of Issues</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rowsLeadTime.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.num}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <br />
+          <br />
+          <Title>Data created: Quality Issues</Title>
+          <br />
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Type of Issue</TableCell>
+                  <TableCell align="right">Number of Issues</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rowsQuality.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.num}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <br />
+          <br />
+          <Title>Data created: Quantity Issues</Title>
+          <br />
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Type of Issue</TableCell>
+                  <TableCell align="right">Number of Issues</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {rowsQuantity.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{row.num}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </React.Fragment>
+      </div>
+    );
+  } else if (data === "Database") {
+    return (
+      <div>
+        <Button variant="outlined" color="primary" onClick={handleClick}>
+          Change data
+        </Button>
+      </div>
+    );
+  }
 }
