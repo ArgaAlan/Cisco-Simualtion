2aea445da5b95706b9332300f5ce7912662bca83
Alberto Labrada <albertolabrada99@gmail.com>
2020-06-24 10:20:56 -0600

    about page route link
    Signed-off-by: Alberto Labrada <albertolabrada99@gmail.com>

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h4>About Page</h4>
+      <h5>Cisco Mexico University Challenge</h5>
+    </div>
+  );
+}
+
+export default About;
